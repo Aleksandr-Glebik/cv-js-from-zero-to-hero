ebae7c5a9d2ec272d2001d5a92155d38da34c990
Aleksandr-Glebik <glebikaa@mail.ru>
2023-01-14 21:35:29 +0300

    feat: add burger menu on width (550 - 320)px

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-console.log('sasha')
+const burgerBtn = document.querySelector('#burger')
+const navList = document.querySelector('.header-nav__list')
+const classNameShow = 'header-nav__list-show'
+const classNameHidden = 'header-nav__list-hidden'
+
+function hiddenOrShowNavBar() {
+    const cls = navList.classList
+    if (cls.contains(classNameShow)) {
+        cls.remove(classNameShow);
+        cls.add(classNameHidden)
+    } else {
+        cls.add(classNameShow);
+        cls.remove(classNameHidden);
+    }
+}
+
+burgerBtn.addEventListener('click', function (event) {
+    hiddenOrShowNavBar()
+})
+
